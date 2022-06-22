1bcfeeed94dd8d3ed33b3fb3a962986b53f63118
bjhobson12 <bjhobson12@gmail.com>
2022-06-22 10:40:23 -0400

    Dropping bash script

--- thread.py
+++ thread.py
@@ -9,33 +9,36 @@ from pathlib import Path
 import sys
 import subprocess
 
-spawn = 0
+
 if len(sys.argv) > 1:
     spawn = int(sys.argv[1])
+    if spawn > 3:
+        exit()
+else:
+    spawn = 0
 
 with open(os.path.join(str(Path.home()), ".stock_trader_keys"), 'r') as keys:
     ENV = {i[0] : i[1] for i in [line.split("=") for line in keys.read().split("\n") if line != ""]}
 
-# Path to thread, arguments, blocking?
-SCRIPT_PATHS = [
-    (os.path.join(str(Path.cwd()), "greeting.py"), None, False),
-    (os.path.join(str(Path.cwd()), "model.py"), None, False)
-]
 TZ = mcal.get_calendar('NYSE').tz
 
+def __run_py_script_process__(name, argstring='', background=False):
+    subprocess.Popen("python3 {} {} >> ./error.log 2>&1 {}".format(os.path.join(os.getcwd(), name), argstring, '&' if background else ''), shell=True)
+
 def __scrape_internet__():
-    subprocess.Popen("python3 {} >> ./error.log 2>&1 &".format(os.path.join(os.getcwd(), 'reddit_scraper.py')), shell=True)
+    __run_py_script_process__('reddit_scraper.py', '{} {} {} {} {}'.format(ENV['client_id'], ENV['client_secret'], ENV['password'], ENV['user_agent'], ENV['username']))
 
 def __init_trader_connection__():
     pass
 
-def __dispatch__():
-    for threads in SCRIPT_PATHS:
-        os.system('python3 {} {}'.format(*threads[:2]))
+def __spawn__():
 
     # new process to update codebase and restart server
-    os.system("chmod +x {}".format(os.path.join(os.getcwd(), 'exec.sh')))
-    subprocess.Popen("{} {} >> ./error.log 2>&1 &".format(os.path.join(os.getcwd(), 'exec.sh'), spawn + 1), shell=True)
+    os.system("git pull")
+    os.system("pip3 install -r requirements.txt")
+    # os.system("chmod +x {}".format(os.path.join(os.getcwd(), 'exec.sh')))
+    # subprocess.Popen("{} {} >> ./error.log 2>&1 &".format(os.path.join(os.getcwd(), 'exec.sh'), spawn + 1), shell=True)
+    __run_py_script_process__('thread.py', str(spawn + 1))
     exit()
 
 def __next_open_bell__():
@@ -50,15 +53,15 @@ def __next_open_bell__():
     return open
 
 def __time_to_opening_bell__():
-    return (__next_open_bell__() - datetime.now(TZ)).seconds
+    return 15 # (__next_open_bell__() - datetime.now(TZ)).seconds
 
-timer = threading.Timer(__time_to_opening_bell__() - 600, __scrape_internet__) # 10 minutes in seconds
+timer = threading.Timer(max(__time_to_opening_bell__() - 600, 0), __scrape_internet__) # 10 minutes in seconds
 timer.start()
 
-timer = threading.Timer(__time_to_opening_bell__() - 120, __init_trader_connection__) # 2 minutes in seconds
+timer = threading.Timer(max(__time_to_opening_bell__() - 120, 0), __init_trader_connection__) # 2 minutes in seconds
 timer.start()
 
-timer = threading.Timer(__time_to_opening_bell__(), __dispatch__)
+timer = threading.Timer(__time_to_opening_bell__(), __spawn__)
 timer.start()
 
 
